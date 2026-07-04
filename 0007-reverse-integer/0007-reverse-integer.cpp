8beca6690959099c93465735d8da4c8e6dd39491
Deepshikha <72968271+deepshikha2812@users.noreply.github.com>
2026-07-04 15:03:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.81%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long result =0 ;
+        while (x!=0){
+            int digit = x%10;
+            x=x/10;
+            result = result * 10 + digit;
+            if(result>INT_MAX || result < INT_MIN){
+                return 0;
+            }
+        }
+        return static_cast<int>(result);
+    }
+};
